c78e3bee5e0205cb52d8f2ef061a0da446e184fa
wangzm <luokf@317.com>
2017-08-09 13:58:59 +0800

    修改server.js

--- server.js
+++ server.js
@@ -1,92 +1,58 @@
-require('./check-versions')()
-
-var config = require('../config')
-if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
+const url = require('url');
+const path = require('path');
+const express = require('express');
+const proxy = require('express-http-proxy');
+const expressWinston = require('express-winston');
+const winston = require('winston'); // for transports.Console
+const helmet = require('helmet');
+const compression = require('compression');
+// 环境变量
+const env = process.env.NODE_ENV || 'testing';
+const isProd = env === 'testing' ? true : false;
+//  APP 配置
+const config = isProd ? require('./config.json')
+  : require('./config.json');
+if (config.watcher) {
+  require(config.watcher);
 }
-
-var opn = require('opn')
-var path = require('path')
-var express = require('express')
-var webpack = require('webpack')
-var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = process.env.NODE_ENV === 'testing'
-  ? require('./webpack.prod.conf')
-  : require('./webpack.dev.conf')
-
-// default port where dev server listens for incoming traffic
-var port = process.env.PORT || config.dev.port
-// automatically open browser, if not set will be false
-var autoOpenBrowser = !!config.dev.autoOpenBrowser
-// Define HTTP proxies to your custom API backend
-// https://github.com/chimurai/http-proxy-middleware
-var proxyTable = config.dev.proxyTable
-
-var app = express()
-var compiler = webpack(webpackConfig)
-
-var devMiddleware = require('webpack-dev-middleware')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  quiet: true
-})
-
-var hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: () => {},
-  heartbeat: 2000
-})
-// force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
+const app = express();
+// 设置安全的 HTTP Header
+app.use(helmet());
+// 开启 gzip 压缩
+app.use(compression());
+// 静态资源
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(
+  expressWinston.logger({
+    transports: [
+      new winston.transports.Console({
+        json: true,
+        colorize: true
+      })
+    ]
   })
-})
-
-// proxy api requests
-Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
-  if (typeof options === 'string') {
-    options = { target: options }
+);
+app.use('/api', proxy(config.api, {
+  proxyReqPathResolver: function (req) {
+    return require('url').parse(req.url).path;
   }
-  app.use(proxyMiddleware(options.filter || context, options))
-})
-
-// handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')())
-
-// serve webpack bundle output
-app.use(devMiddleware)
-
-// enable hot-reload and state-preserving
-// compilation error display
-app.use(hotMiddleware)
-
-// serve pure static assets
-var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
-
-var uri = 'http://localhost:' + port
-
-var _resolve
-var readyPromise = new Promise(resolve => {
-  _resolve = resolve
-})
-
-console.log('> Starting dev server...')
-devMiddleware.waitUntilValid(() => {
-  console.log('> Listening at ' + uri + '\n')
-  // when env is testing, don't need open it
-  if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri)
+}));
+app.use('/privilegeApi', proxy(config.privilegeApi, {
+  proxyReqPathResolver: function (req) {
+    return require('url').parse(req.url).path;
   }
-  _resolve()
-})
-
-var server = app.listen(port)
-
-module.exports = {
-  ready: readyPromise,
-  close: () => {
-    server.close()
-  }
-}
+}));
+// express-winston errorLogger makes sense AFTER the router.
+app.use(
+  expressWinston.errorLogger({
+    transports: [
+      new winston.transports.Console({
+        json: true,
+        colorize: true
+      })
+    ]
+  })
+);
+app.listen(config.port, () => {
+  console.log(`🌎  => Server is running on port ${config.port}`);
+});
